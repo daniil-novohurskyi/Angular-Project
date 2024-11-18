783b45da6eaeee4eb9367cc8f3aed4c4d3272276
s28328 <s28328@pjwstk.edu.pl>
2024-11-18 20:27:13 +0100

    Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {DropdownDirective} from '../shared/dropdown.directive';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    DropdownDirective
+  ],
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+}
